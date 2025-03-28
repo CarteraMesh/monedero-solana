90037804e92a40ef42a9f2a738d9daf5e3ad6de1
Doug Chimento <dchimento@gmail.com>
2025-03-28 10:56:25 +0000

    native jup-ag

--- jupiter/src/error.rs
+++ jupiter/src/error.rs
@@ -1,5 +1,23 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error(transparent)]
-    QuoteError(#[from] jup_ag::Error),
+    #[error("reqwest: {0}")]
+    Reqwest(#[from] reqwest::Error),
+
+    #[error("invalid pubkey in response data: {0}")]
+    ParsePubkey(#[from] solana_sdk::pubkey::ParsePubkeyError),
+
+    #[error("base64: {0}")]
+    Base64Decode(#[from] base64::DecodeError),
+
+    #[error("bincode: {0}")]
+    Bincode(#[from] bincode::Error),
+
+    #[error("Jupiter API: {0}")]
+    JupiterApi(String),
+
+    #[error("serde_json: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+
+    #[error("parse SwapMode: Invalid value `{value}`")]
+    ParseSwapMode { value: String },
 }
